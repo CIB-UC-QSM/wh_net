7a9fcdc5860b7b1d27f5a35f1944e8fec4f4be93
mglambert <mglambert@uc.cl>
2026-06-27 22:29:17 -0400

    gen additional 2

--- gen_data.py
+++ gen_data.py
@@ -21,7 +21,7 @@ def process_3d_volume(image):
     cropped_image = image[z_min:z_max, y_min:y_max, x_min:x_max]
     cropped_mask = mask[z_min:z_max, y_min:y_max, x_min:x_max]
 
-    target_size = np.random.randint(100, 160, 3)
+    target_size = np.random.randint(80, 160, 3)
     
     img_zoom = tuple(target_size[i] / s for i, s in enumerate(cropped_image.shape))
     resized_image = zoom(cropped_image, img_zoom, order=3)
@@ -32,7 +32,8 @@ def process_3d_volume(image):
     delta_a = 160 - target_size[0]
     delta_b = 160 - target_size[1]
     delta_c = 160 - target_size[2]
-    pad_width = ((delta_a//2, delta_a-delta_a//2), (delta_b//2, delta_b-delta_b//2), (delta_c//2, delta_c-delta_c//2))
+    n = np.random.randint(1, 5, 3)
+    pad_width = ((delta_a//n[0], delta_a-delta_a//n[0]), (delta_b//n[1], delta_b-delta_b//n[1]), (delta_c//n[2], delta_c-delta_c//n[2]))
     final_image = np.pad(resized_image, pad_width, mode='constant', constant_values=0)
     final_mask = np.pad(resized_mask, pad_width, mode='constant', constant_values=0)
     
@@ -52,8 +53,8 @@ for i in tqdm(range(105)):
     img = nii.load(f'qsm_data/sub-{i:04d}_ses-1_acq-wb_mod-qsm_orient-std_brain.nii.gz').get_fdata()
     
     img, mask = process_3d_volume(img)
-    image_file = f'train_data/image_{105+i:04d}.pt'
-    mask_file = f'train_data/mask_{105+i:04d}.pt'
+    image_file = f'train_data/image_{2*105+i:04d}.pt'
+    mask_file = f'train_data/mask_{2*105+i:04d}.pt'
     print(mask.shape)
     imshow_3d(img*mask, '', rango=(-0.1, 0.1))
     break
